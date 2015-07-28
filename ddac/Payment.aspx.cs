700810ba944e6eb14e49e77703eac0ea1d53ffe8
breenatheseira <breenatheseira@yahoo.com>
2015-07-29 01:41:03 +0800

    Able to set form values based on URL parameters and send them to Paypal website

--- ddac/Payment.aspx.cs
+++ ddac/Payment.aspx.cs
@@ -9,9 +9,25 @@ namespace ddac
 {
     public partial class Payment : System.Web.UI.Page
     {
+        public String item_name {get; set;}
+        public String total_price { get; set; }
+        public String email { get; set; }
+        public String website { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            // to be removed once booking is done with redirecting params
+            if (string.IsNullOrEmpty((String)Request.Params["bookingID"])) 
+                Response.Redirect("Payment.aspx?bookingID=1&item_name=Asia Pacific&cabin=Oceanview&total=5000");
 
+            String booking = (String)Request.Params["bookingID"];
+            String item = (String)Request.Params["item_name"];
+            String cabin = (String)Request.Params["cabin"];
+            String total = (String)Request.Params["total"];
+
+            item_name = "BookingID: " + booking + " - Region: " + item + " using Cabin Type: " + cabin;
+            total_price = total;
+            email = "breenatheseira-facilitator@yahoo.com";
+            website = "https://www.sandbox.paypal.com/cgi-bin/webscr";
         }
     }
 }

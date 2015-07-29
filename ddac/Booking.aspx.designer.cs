6499663e68683337c53167076f663551c9a64d9c
breenatheseira <breenatheseira@yahoo.com>
2015-07-29 15:12:17 +0800

    Added:Booking and SendGrid for real this time

--- ddac/Booking.aspx.designer.cs
+++ ddac/Booking.aspx.designer.cs
@@ -101,5 +101,14 @@ namespace ddac {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DataList CabinList;
+        
+        /// <summary>
+        /// BookButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BookButton;
     }
 }
